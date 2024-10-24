e45ac049beca10eeb488d5a078095d7b4b717399
panyou1996 <131009720+panyou1996@users.noreply.github.com>
2024-10-24 22:16:10 +0800

    Update notion-hugo.config.ts
    update my url

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://encouraging-hide-254.notion.site/Notion-DoIt-129ec829faf280a08084c102c4119962?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
